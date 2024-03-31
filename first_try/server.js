c9036cfb6f0fa0fe4e2bc90c7ead88ab2534b072
unichoro <ruilim@unist.ac.kr>
2024-03-31 23:52:51 +0900

    server request post exercise

--- first_try/server.js
+++ first_try/server.js
@@ -7,8 +7,15 @@ var querystring = require('querystring');
 
 var server = http.createServer(function(request,response){
 
+    console.log('--- log start ---')
+    var parsedUrl = url.parse(request.url);
+    console.log(parsedUrl);
+    var parsedQuery = querystring.parse(parsedUrl.query, '&', '=');
+    console.log(parsedQuery);
+    console.log('--- log end --');
+
     response.writeHead(200,{'Content-Type' : 'text/ html'});
-    response.end('Hellow node.js!!');
+    response.end('var1의 값은 '+parsedQuery.var1);
 });
 
 server.listen(8080, function(){
---
+++ first_try/serverPost.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var querystring = require('querystring');
+
+var server = http.createServer(function(request,response){
+    var postdata = '';
+    request.on('data', function(data) {
+        postdata=postdata+data;
+    });
+
+    request.on('end', function() {
+        var querystring = querystring.parse(postdata);
+        console.log(querystring);
+        response.writeHead(200, {'Content-Type':'text/html'})
+        response.end('value of val1='+ result);
+    })
+});
+
+server.listen(8000, function(){
+    console.log('Server is running....')
+})
